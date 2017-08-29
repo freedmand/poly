d5a67efa07f72a00fb6efdecfd9ba9a93de40d9f
freedmand <freedmand@gmail.com>
2017-08-29 10:11:28 -0700

    Import checker now works and prints out cycles successfully. The current codebase is free of cycles

--- import_checker.py
+++ import_checker.py
@@ -1,6 +1,13 @@
-# Incomplete Python script that checks JS files for circular imports.
+# Python script that checks JS files for circular imports and prints cycles
+# using Tarjan's algorithm. If it does not print anything, the code is free of
+# circular imports.
+#
+# Usage:
+#   python import_checker.py
 
 import os, re
+from collections import defaultdict
+from itertools import chain
 
 ENTRY_POINT = 'lib/run_tests.js'
 
@@ -12,24 +19,71 @@ def getImports(fn):
     return [os.path.normpath(os.path.join(dirname, importFile)) for
             importFile in imports]
 
-def add(_, dirname, names):
-  for name in names:
-    if name.endswith('.js'):
-      fn = os.path.join(dirname, name)
-      print fn
-      for importFile in getImports(fn):
-        print ' ', os.path.normpath(os.path.join(dirname, importFile))
-
-importDepths = {}
-
-def printImports(fn, spaces=0):
-  print (' ' * spaces) + fn
-  imports = getImports(fn)
+# Returns all the edges in the graph.
+def getEdges(startingFn, visited=None):
+  if visited is None:
+    visited = defaultdict(bool)
+  imports = getImports(startingFn)
+  edges = []
   for i in imports:
-    if i in importDepths and importDepths[i] < spaces:
-      print ' ' * (spaces + 2) + i + '(i)'
-    else:
-      importDepths[i] = spaces
-      printImports(i, spaces + 1)
+    edges += [(startingFn, i)]
+    if not visited[i]:
+      visited[i] = True
+      edges += getEdges(i, visited)
+  return edges
 
-printImports(ENTRY_POINT)
+# from https://gist.github.com/JonasGroeger/5459190
+class Graph(object):
+    def __init__(self, edges, vertices=()):
+        edges = list(list(x) for x in edges)
+        self.edges = edges
+        self.vertices = set(chain(*edges)).union(vertices)
+        self.tails = defaultdict(list)
+        for head, tail in self.edges:
+            self.tails[head].append(tail)
+
+    @classmethod
+    def from_dict(cls, edge_dict):
+        return cls((k, v) for k, vs in edge_dict.items() for v in vs)
+
+class _StrongCC(object):
+    def strong_connect(self, head):
+        lowlink, count, stack = self.lowlink, self.count, self.stack
+        lowlink[head] = count[head] = self.counter = self.counter + 1
+        stack.append(head)
+
+        for tail in self.graph.tails[head]:
+            if tail not in count:
+                self.strong_connect(tail)
+                lowlink[head] = min(lowlink[head], lowlink[tail])
+            elif count[tail] < count[head]:
+                if tail in self.stack:
+                    lowlink[head] = min(lowlink[head], count[tail])
+
+        if lowlink[head] == count[head]:
+            component = []
+            while stack and count[stack[-1]] >= count[head]:
+                component.append(stack.pop())
+            self.connected_components.append(component)
+
+    def __call__(self, graph):
+        self.graph = graph
+        self.counter = 0
+        self.count = dict()
+        self.lowlink = dict()
+        self.stack = []
+        self.connected_components = []
+
+        for v in self.graph.vertices:
+            if v not in self.count:
+                self.strong_connect(v)
+
+        return self.connected_components
+
+strongly_connected_components = _StrongCC()
+
+edges = getEdges(ENTRY_POINT)
+for edgeGroup in strongly_connected_components(Graph(edges)):
+  if len(edgeGroup) == 1:
+    continue
+  print edgeGroup
